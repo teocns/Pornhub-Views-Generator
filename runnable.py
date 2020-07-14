7c94b6c54215b8c32fae2d0edbb796f2ec9d92e1
whydoyoucare <teocns@gmail.com>
2020-07-14 02:45:19 +0200

    Everything

--- runnable.py
+++ runnable.py
@@ -3,12 +3,17 @@ import random
 import requests
 from proxy_provider import get_proxy_dict,generate_session_id
 from time import sleep
-
+from multiprocessing.dummy import Pool as ThreadPool
+from threading import Timer, Event
 
 
 REFERERS_LIST = []
 with open('referers.txt','r') as f:
     REFERERS_LIST = [line.strip() for line in f.read().split("\n")]
+    
+UA_LIST = []
+with open('ua.txt','r') as f:
+    UA_LIST = [line.strip() for line in f.read().split("\n")]
     
 
 TOTAL_VIDEO_COUNTS = 0
@@ -23,18 +28,23 @@ def test(i):
     session = requests.Session()
 
     VIDEO_URL = 'https://www.pornhub.com/view_video.php?viewkey=ph5f0cc6468692e'
+    UA = random.choice(UA_LIST)
     REFERER = random.choice(REFERERS_LIST)
     video_loaded_response = session.get(
         VIDEO_URL,
         proxies= proxydict,
         headers={
-            'referer':REFERER
+            'referer':REFERER,
+            'user-agent':UA
         }
     )
     if video_loaded_response.status_code != 200:
         print('Something went wrong!')
 
-    cookieJar = video_loaded_response.cookies
+
+    """data-video-id="332754762">"""
+
+    VIDEO_ID = str(re.findall('(?<=data-video-id=").+?(?=")',video_loaded_response.text)[0])
     # Wait 8 seconds...
     print ('Sleeping 8 seconds')
     sleep(7)
@@ -45,7 +55,10 @@ def test(i):
     final_response = session.get(
         f"https://www.pornhub.com{register_video_count_url}",
         headers={
-            'referer':VIDEO_URL
+            'referer':VIDEO_URL,
+            'host':'www.pornhub.com',
+            'origin':'https://www.pornhub.com',
+            'user-agent':UA
         },
         proxies=proxydict
     )
@@ -54,9 +67,28 @@ def test(i):
         print('Video count registered. Total = '+str(TOTAL_VIDEO_COUNTS))
     
 
-rng = range(0,99999)
+    LIKE_URL = register_video_count_url = str(re.findall('(?<=submitVote":").+?(?=")',video_loaded_response.text)[0]).replace('\\','')
+    # Put a like aswell
+    like_delay = random.randint(10,40)
+    print('Placing like')
+    
+    sleep(like_delay)
+    session.post(
+        f"https://www.pornhub.com{LIKE_URL}",
+        headers={
+            'referer':VIDEO_URL,
+            'host':'www.pornhub.com',
+            'origin':'https://www.pornhub.com',
+            'user-agent':UA
+        },  
+        proxies=proxydict
+    )
 
-from multiprocessing.dummy import Pool as ThreadPool
-pool = ThreadPool(2)
 
-pool.map(test,rng)
+rng = range(0,999999)
+
+
+
+with  ThreadPool(34) as pool:
+    pool.map(test,rng)
+
